edf152af95c9979348f6641928bd73dda9c42821
Rahim1412 <hadjiatabderrahim14@gmail.com>
2025-10-30 10:33:11 +0100

    Update test.py

--- LeptonModule/test.py
+++ LeptonModule/test.py
@@ -4,10 +4,10 @@ cam = CameraIR()
 
 cam.start_cam()
 
-frame = capture_lepton_frame("/dev/video1")  # retourne une image numpy (grayscale uint8)
+frame = cam.capture_lepton_frame("/dev/video1")  # retourne une image numpy (grayscale uint8)
 print(frame.shape, frame.dtype)              # ex: (120, 160) uint8 pour Lepton
 
 # (optionnel) sauvegarder pour vérifier
-cv2.imwrite("/home/pi/test_lepton.jpg", frame)
+cv2.imwrite("/home/dracofeu/dracofeu_IOT/LeptonModule/test_lepton.jpg", frame)
 
 cam.stop_cam()
