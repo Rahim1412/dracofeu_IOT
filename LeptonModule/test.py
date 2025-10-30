86486b9c6257e48ab7fe55d331cdc1571710ef91
Rahim1412 <hadjiatabderrahim14@gmail.com>
2025-10-30 11:13:43 +0100

    Update test.py

--- LeptonModule/test.py
+++ LeptonModule/test.py
@@ -4,6 +4,7 @@ cam = CameraIR()
 
 cam.start_cam()
 
+print("Capture image Lepton...")
 frame = cam.capture_lepton_frame("/dev/video1")  # retourne une image numpy (grayscale uint8)
 print(frame.shape, frame.dtype)              # ex: (120, 160) uint8 pour Lepton
 
