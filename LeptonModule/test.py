91c07651aae953531b27f50c4330f898c2c4e1e1
Rahim1412 <hadjiatabderrahim14@gmail.com>
2025-11-14 15:49:19 +0100

    Update test.py

--- LeptonModule/test.py
+++ LeptonModule/test.py
@@ -6,8 +6,10 @@ cam.start_cam()
 for i in range(5):
     print(f"📸 Photo {i+1}/100 ...")
     cam.capture_image()   # ta fonction qui sauvegarde automatiquement avec un numéro
+
+    print("attendre 1 seconde...")
     sleep(1)  # Petite pause entre les captures
-                        
+    
 print("✔️ 5 photos prises.")
 cam.stop_cam()
 
