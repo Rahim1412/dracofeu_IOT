9af7c525ab97b01a4bd0c09d6588aa42463c5128
Rahim1412 <hadjiatabderrahim14@gmail.com>
2025-11-14 16:22:38 +0100

    Update capture.py

--- LeptonModule/capture.py
+++ LeptonModule/capture.py
@@ -4,10 +4,10 @@ import os
 device = "/dev/video1"
 save_dir = "/home/dracofeu/dracofeu_IOT/LeptonModule/capture"
 os.makedirs(save_dir, exist_ok=True)
-save_path = os.path.join(save_dir, "test_opencv.jpg")
+save_path = os.path.join(save_dir, "test_opencv_ffmpeg.jpg")
 
-print(f"[INFO] Ouverture de {device} ...")
-cap = cv2.VideoCapture(device, cv2.CAP_V4L2)  # on force V4L2
+print(f"[INFO] Ouverture de {device} avec CAP_FFMPEG ...")
+cap = cv2.VideoCapture(device, cv2.CAP_FFMPEG)  # 🔴 ici on force FFMPEG
 
 if not cap.isOpened():
     print("❌ cap.isOpened() = False")
@@ -26,4 +26,4 @@ if not ret or frame is None:
 print(f"[INFO] frame.shape = {frame.shape}, dtype = {frame.dtype}")
 
 ok = cv2.imwrite(save_path, frame)
-print("✅ Image sauvegardée ?" , ok, "→", save_path)
+print("✅ Image sauvegardée ?", ok, "→", save_path)
