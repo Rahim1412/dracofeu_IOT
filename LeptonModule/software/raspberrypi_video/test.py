7d092c4dbce977596137e64920fdb87717abc83d
Rahim1412 <hadjiatabderrahim14@gmail.com>
2025-12-05 00:12:10 +0100

    Update test.py

--- LeptonModule/software/raspberrypi_video/test.py
+++ LeptonModule/software/raspberrypi_video/test.py
@@ -1,4 +1,6 @@
 from CameraIR import CameraIR
+import cv2
+import time
 
 cam = CameraIR()
 cam.start_cam()
@@ -6,9 +8,11 @@ cam.start_cam()
 while True:
     frame = cam.capture_frame()
     if frame is None:
+        time.sleep(0.01)
         continue
 
     cv2.imshow("Flux IR (via CameraIR)", frame)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
