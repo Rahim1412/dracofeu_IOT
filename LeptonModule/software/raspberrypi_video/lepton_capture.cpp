1b93b6438b9d45a54650a7eaee8fc3725753a3eb
Rahim1412 <hadjiatabderrahim14@gmail.com>
2025-12-05 12:40:51 +0100

    Update lepton_capture.cpp

--- LeptonModule/software/raspberrypi_video/lepton_capture.cpp
+++ LeptonModule/software/raspberrypi_video/lepton_capture.cpp
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
     // ----------- Lecture optionnelle d'une plage en °C ----------
     bool useTempRange = true;
     double tempMinC = 20;
-    double tempMaxC = 60;
+    double tempMaxC = 40;
 
     for (int i = 1; i < argc; ++i) {
         if (std::strcmp(argv[i], "--tempC") == 0 && i + 2 < argc) {
